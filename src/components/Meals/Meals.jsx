737782cffe746970cf5894b9add0c282b372c555
Ahmed Frag <a.frag166@gmail.com>
2025-02-01 21:43:58 +0200

    Components: Header & Meals (fetching meals data in Meals component)

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const Meals = () => {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+        const response = await fetch(`http://localhost:3000/meals`);
+        if (!response.ok) {
+          // ...
+        }
+    
+        const meals = await response.json();
+        setLoadedMeals(meals)
+      };
+
+      fetchMeals()
+  }, [])
+
+  console.log(loadedMeals);
+
+  return <ul id="meals"></ul>;
+};
+
+export default Meals;
